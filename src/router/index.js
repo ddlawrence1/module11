30ecbb9bd99ccd92544f5819f8730a8c283ac77e
ddlawrence1 <lawrencedestiny88@gmail.com>
2025-11-10 19:32:28 -0500

    Rename About component and update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'; 
-import VueRouter from 'vue-router'; 
-import Home from '../views/Home.vue'; 
-import About from '../views/About.vue'; 
- 
-Vue.use(VueRouter); 
-const routes = [ 
-  { 
-      path: '/', name: 'Home', component: Home 
-  }, 
-  { 
-      path: '/about', name: 'About', component: About 
-  } 
-]; 
-const router = new VueRouter({ 
-  mode: 'history', routes 
-}); 
-export default router; 
- 
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import AboutPage from '../views/About.vue'   // <-- import your renamed component
+import RandomQuotes from '../views/RandomQuotes.vue'
+import Contact from '../views/Contact.vue'
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'About', component: AboutPage },   // <-- use the imported component
+  { path: '/quotes', name: 'RandomQuotes', component: RandomQuotes },
+  { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound } // 404 route
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
--- src/views/About.vue
+++ src/views/About.vue
@@ -9,6 +9,6 @@
 
 <script>
 export default {
-  name: 'About'
+  name: "AboutPage",  
 }
 </script>
